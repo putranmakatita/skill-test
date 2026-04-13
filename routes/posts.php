58e3afb72a92466185d84f609eddbcfd5be1e1d5
Git <lina@rcelectronic.co.id>
2026-04-13 18:54:42 +0700

    use resource

--- routes/posts.php
+++ routes/posts.php
@@ -3,20 +3,5 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-// Public List
-Route::get('posts', [PostController::class, 'index'])->name('posts.index');
-
-// Auth Protected
-Route::middleware(['auth'])->group(function () {
-    Route::get('posts/create', [PostController::class, 'create'])->name('posts.create');
-    Route::post('posts', [PostController::class, 'store'])->name('posts.store');
-
-    // Authorization-required routes
-    Route::get('posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-    Route::put('posts/{post}', [PostController::class, 'update'])->name('posts.update');
-    Route::patch('posts/{post}', [PostController::class, 'update']);
-    Route::delete('posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
-});
-
-// Wildcard Public (Must be LAST)
-Route::get('posts/{id}', [PostController::class, 'show'])->name('posts.show');
+Route::resource('posts', PostController::class)
+    ->middleware(['auth' => ['except' => ['index', 'show']]]);
